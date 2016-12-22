ee012b9c18956ba1e30b700938e9384d82ab07e3
skullkeeper94 <gabubble2002@live.com>
2016-12-22 00:52:09 -0500

    Create R.java

---
+++ gen/com/skullkeeper94/permissionlist/R.java
@@ -0,0 +1,9 @@
+package com.skullkeeper94.permissionlist;
+
+public final class R {
+    public static final class attr {
+    }
+    public static final class string {
+        public static final int app_name=0x7f020000;
+    }
+}
