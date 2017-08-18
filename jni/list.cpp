9bf9b187d33aa35710a71983f74350b67b13230b
skullkeeper94 <gabubble2002@live.com>
2017-08-18 13:19:30 -0400

    Update list.cpp

--- jni/list.cpp
+++ jni/list.cpp
@@ -5,6 +5,6 @@ extern "C"
 {
 	JNIEXPORT jstring JNICALL Java_com_skullkeeper94_permissionlist_list_stringFromJNI(JNIEnv* env, jobject thiz)
 	{
-		return env->NewStringUTF("TODO: Add categories before adding seperate tabs for the permissions.");
+		return env->NewStringUTF("How does one add categories?");
 	}
 }
